2b48b2674728830cde6ceb576dbc6a35e7d18241
Peter Zhang <baiyan.zhang21@student.xjtlu.edu.cn>
2024-04-23 17:49:39 +0800

    About user information

---
+++ booking4sportcentre/src/main/java/com/group6/booking4sportcentre/mapper/UserInfoMapper.java
@@ -0,0 +1,10 @@
+package com.group6.booking4sportcentre.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.group6.booking4sportcentre.model.UserInfo;
+
+@Mapper
+public interface UserInfoMapper extends BaseMapper<UserInfo>{
+}
